7797eca1179f1c41ab6f357a7c5d40f0572e7637
Hamza <HamzaKhan2702@outlook.com>
2025-12-04 22:13:37 +0000

    Registering primary schools
    - Found data file with a LOT of primary schools
    - Created a command to import primary schools
    - Updated UserController to store school_id when registering user
    - Added school_id as a foreign key
    - Created school table to get the data from the CSV
    - Registering now has a select input to get a primary school

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
         'password',
+        'school_id',
         'pfp',
     ];
 
